e060af8d5b622ec30c6664bfe44bd3ddfeecf24f
jandie <eidnaj@live.nl>
2018-05-19 19:46:19 +0200

    Now really works with cloud pws

--- src/main/java/com/jandiehendriks/kwetter/KwetterApplication.java
+++ src/main/java/com/jandiehendriks/kwetter/KwetterApplication.java
@@ -7,7 +7,9 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication
+@Configuration
+@ComponentScan
+@EnableAutoConfiguration
 public class KwetterApplication {
 
 	public static void main(String[] args) {
